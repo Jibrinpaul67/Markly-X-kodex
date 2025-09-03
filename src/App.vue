e178ab8e9b3f2a092dff9fc525cd1f212a5a3376
jibrin-admin <jibriinesther@gmail.com>
2025-09-03 13:39:35 +0100

    deux

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  components: { HelloWorld }
+};
+</script>
+
+<template>
+  <div>
+    <h1>Main App</h1>
+    <HelloWorld />
+  </div>
+</template>
