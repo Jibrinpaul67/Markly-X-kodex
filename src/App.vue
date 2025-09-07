5458fc920d8896dcd018fc0fe7085eecb8f81e93
jibrin-admin <jibriinesther@gmail.com>
2025-09-07 12:25:46 +0100

    images

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@
 
     <!-- ✅ Hero Section -->
     <section class="hero">
+    <img src="./assets/Screenshot_2025-09-07_105537-removebg-preview.png"/>
       <h1>Markly Bags</h1>
       <p>   We believe in the power of a well-crafted bag to enhance your lifestyle.
        <br> Explore our curated collection of premium bags that seamlessly blend style,
@@ -22,19 +23,87 @@
     <!-- ✅ Belief Section -->
     <section class="belief">
       <div class="belief-text">
-        <h2>We believe in fashion</h2>
+      <h6> YOUR PERFECT FIT</h6>
+        <h2>We believe in fashion that cares for the planet. Our commitment to
+         sustainability goes beyond creating sylish bags-it's about 
+         making a positive impact.</h2>
         <p>
-          At Markly Bags, we believe fashion is more than just a statement —
-          it’s a lifestyle.<br> Our designs are crafted to combine elegance,
-          durability, and functionality, ensuring you stand out while staying
-          true to yourself.
+         Choosing the right size is crucial when it comes to bags. Use our comprehensive
+         size guide to ensure your Markly bag is not only stylish but also perfectly suited
+         to your needs. From compact crossbodies to spacious totes, we've got the right 
+         size for every occasion.
         </p>
       </div>
       <div class="belief-image">
-               <img src="./assets/blackbag.png" alt="Bag" />
+               <img src="./assets/blackbag.png" alt="Bag" style="width: 500px; height: auto;"  />
       </div>
     </section>
-    
+
+    <!-- ✅ Adventure Section -->
+    <section class="advent">
+     <div class="advent-image">
+               <img src="./assets/beadraindrops.png" alt="Bag" style="width: 500px; height: auto;" />
+      </div>
+      <div class="advent-text">
+        <h2>Adventure collection is designed to keep up with your dynamic lifestyle,
+        offering durability without compromising on style.</h2>
+        <p>
+          At Markly, we believe in the power of a well-crafted bag to enhance your lifestyle.<br>
+          Explore our curated collection of premium bags that seamlessly blend style,<br>
+          functionality, and durability.
+        </p>
+      </div>
+    </section>
+
+  
+    <!-- ✅ Pink Section -->
+    <section class="pink">
+     <div class="advent-image">
+               <img src="./assets/Screenshot_2025-09-07_105214-removebg-preview.png" style="width: 500px; height: auto;" />
+      </div>
+      <div class="pink-text">
+        <h2> Whether you're seeking classic sophistication, urban chic, or a companion for you, Markly
+        has the perfect bag for every occasion.</h2>
+        <p>
+          Discover our current best-sellers and customer favorites.Each product is a testament to Markly's
+          commitment to combining style, durability, and functionality.
+        </p>
+      </div>
+    </section>
+
+     <!-- ✅ choose Section -->
+    <section class="choose">
+      <div class="choose-text">
+        <h2>Why Choose Markly?</h2>
+        <p>
+          Discover our current best-sellers and customer favorites.Each product is a testament to <br>
+          Markly's commitment to combining style, durability, and functionality.
+        </p>
+        </div>
+      <div class="choose-container">
+      <div class="choose-image">  
+      <img src="./assets/Screenshot 2025-09-07 113259.png" style="width:300px;"/>
+      <h3>Quality Craftsmanship</h3>
+      <p> Meticulously crafted using premium materials, Our bags are a testament to enduring quality,
+      ensuring you're always prepared for any journey.</p>
+      </div>
+      <div class="choose-image"> 
+       <img src="./assets/Screenshot 2025-09-07 113321.png" style="width:300px;"/>
+       <h3>Versatility</h3>
+      <p> From the office to weekend getaways, Markly bags seamlessly blend style with practicality,
+      ensuring you're always prepared fro any journey.
+      </p>
+      </div>
+      <div class="choose-image"> 
+       <img src="./assets/Screenshot 2025-09-07 113338.png" style="width:300px;" />
+       <h3>Sustainability</h3>
+      <p> We're committed to eco-friendly practices, sourcing materials responsibly and 
+      crafting bags that stand the test of time </p>
+      </div>
+      </div>
+    </section>
+
+
   </div>
 </template>
 
